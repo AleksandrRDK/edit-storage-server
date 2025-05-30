305fe58f4f34582388e7e6e8232ed3d656289814
Alexandr Rudakov <aleksandrrdk.code@gmail.com>
2025-05-30 03:49:34 +0300

    add time reshenie

--- index.js
+++ index.js
@@ -20,6 +20,20 @@ app.use(
     })
 );
 
+// 🛡️ Временное решение: ручные заголовки (на всякий случай)
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', 'https://aleksandrrdk.github.io');
+    res.header(
+        'Access-Control-Allow-Headers',
+        'Origin, X-Requested-With, Content-Type, Accept'
+    );
+    res.header(
+        'Access-Control-Allow-Methods',
+        'GET, POST, PUT, DELETE, OPTIONS'
+    );
+    next();
+});
+
 app.use(express.json());
 
 // Подключение к MongoDB
